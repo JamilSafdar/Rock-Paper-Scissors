308c49ea14a4d32f5046a03de9791442fba35a35
Jamil Safdar <jamil.safdar@aptitudesoftware.com>
2023-07-02 09:54:17 +0100

    chore: removed Console throughout file by incuding it in using statement

--- RockPaperScissors/Program.cs
+++ RockPaperScissors/Program.cs
@@ -1,4 +1,4 @@
-﻿
+﻿using static System.Console;
 namespace RockPaperScissors
 {
     partial class Program
@@ -17,14 +17,14 @@ namespace RockPaperScissors
                 if (wins == 3 || losses == 3)
                     break;
 
-                Console.WriteLine("--------------------");
-                Console.WriteLine("LETS PLAY!");
-                Console.WriteLine("\nRound: {0}", turn);
-                Console.WriteLine("--------------------");
-                Console.WriteLine("First to win 3 rounds is the winner!");
-                Console.WriteLine("Please take your turn: " + string.Join(", ", rps.Selection) + ", or press 'q' to quit the game");
-                Console.WriteLine("");
-                string selection = Console.ReadLine().Trim().ToLower();
+                WriteLine("--------------------");
+                WriteLine("LETS PLAY!");
+                WriteLine("\nRound: {0}", turn);
+                WriteLine("--------------------");
+                WriteLine("First to win 3 rounds is the winner!");
+                WriteLine("Please take your turn: " + string.Join(", ", rps.Selection) + ", or press 'q' to quit the game");
+                WriteLine("");
+                string selection = ReadLine().Trim().ToLower();
 
                 if (selection == "q")
                     break;
@@ -32,70 +32,68 @@ namespace RockPaperScissors
                 if (!rps.Selection.Contains(selection))
 
                 {
-                    Console.WriteLine("Invalid choice!");
+                    WriteLine("Invalid choice!");
                     continue;
                 }
 
                 int result = rps.Next(selection);
 
-                Console.WriteLine("");
-                Console.WriteLine("You chose {0} and your opponent chose {1}!", selection, rps.LastCPUSelection);
+                WriteLine("");
+                WriteLine("You chose {0} and your opponent chose {1}!", selection, rps.LastCPUSelection);
 
                 switch (result)
 
                 {
                     case 1:
-                        Console.WriteLine("");
-                        Console.WriteLine("{0} beats {1} - You WON this round.", selection, rps.LastCPUSelection);
+                        WriteLine("");
+                        WriteLine("{0} beats {1} - You WON this round.", selection, rps.LastCPUSelection);
                         wins++;
                         turn++;
                         mostUsedMoves.Add(selection);
                         break;
                     case 0:
 
-                        Console.WriteLine("");
-                        Console.WriteLine("This round is a DRAW!");
+                        WriteLine("");
+                        WriteLine("This round is a DRAW!");
                         draws++;
                         turn++;
                         mostUsedMoves.Add(selection);
                         break;
                     case -1:
-                        Console.WriteLine("");
-                        Console.WriteLine("{0} beats {1} - You LOST this round", rps.LastCPUSelection, selection);
+                        WriteLine("");
+                        WriteLine("{0} beats {1} - You LOST this round", rps.LastCPUSelection, selection);
                         losses++;
                         turn++;
                         mostUsedMoves.Add(selection);
                         break;
                 }
 
-                Console.WriteLine("");
-                Console.WriteLine("Do you want to play again? Press 'y' for Yes or 'n' for No");
-                if (!Console.ReadLine().StartsWith("y", StringComparison.OrdinalIgnoreCase)) break;
+                WriteLine("");
+                WriteLine("Do you want to play again? Press 'y' for Yes or 'n' for No");
+                if (!ReadLine().StartsWith("y", StringComparison.OrdinalIgnoreCase)) break;
             }
-            Console.WriteLine("-------------------------------------------------");
-            Console.WriteLine("MATCH RESULT - Win 3 to win the game.");
-            Console.WriteLine("-------------------------------------------------");
+            WriteLine("-------------------------------------------------");
+            WriteLine("MATCH RESULT - Win 3 to win the game.");
+            WriteLine("-------------------------------------------------");
             turn--;
-            Console.WriteLine("The game was completed in {0} turns", turn);
+            WriteLine("The game was completed in {0} turns", turn);
 
             var mostUsedMove = mostUsedMoves
                 .GroupBy(move => move)
                 .OrderByDescending(group => group.Count())
                 .First()
                 .Key;
+            WriteLine("The most used move was {0}", mostUsedMove);
 
-            Console.WriteLine("The most used move was {0}", mostUsedMove);
-
-            Console.WriteLine("-------------------------------------------------");
+            WriteLine("-------------------------------------------------");
             if (losses == 3 && wins < 3)
-                Console.WriteLine("You Lost the Match!");
+                WriteLine("You Lost the Match!");
 
             else if (wins == 3 && losses < 3)
-                Console.WriteLine("You Won the Match!");
+                WriteLine("You Won the Match!");
 
             else
-                Console.WriteLine("Game Ended with no overall winner - 3 wins required");
-
+                WriteLine("Game Ended with no overall winner - 3 wins required");
         }
     }
 }
